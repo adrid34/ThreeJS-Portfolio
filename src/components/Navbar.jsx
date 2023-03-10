d53ca311f44094074fa0e8f63da2c6b5cab605ff
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-10 23:18:38 +0100

    first elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles';
+import { navLinks } from '../constants/constants'
+import {logo, menu, close} from '../assets'
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+
+      <nav className={`${styles.paddingX} fixed top-0 w-full items-center py-5 z-20`}>
+        
+        <div className='w-full flex justify-between items-center mx-auto max-w-6xl'>
+            <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive('') }} ></Link>
+        </div>
+
+      </nav>
+
   )
 }
 
