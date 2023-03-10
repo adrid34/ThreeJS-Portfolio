0cddf3b7a4fb0c94432e6df72a1f9f193ea31615
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-10 23:20:39 +0100

    Initial state for active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styles } from '../styles';
 import { navLinks } from '../constants/constants'
@@ -6,12 +6,14 @@ import {logo, menu, close} from '../assets'
 
 const Navbar = () => {
 
+  const [active, setActive] = useState('');
+
   return (
 
       <nav className={`${styles.paddingX} fixed top-0 w-full items-center py-5 z-20`}>
         
         <div className='w-full flex justify-between items-center mx-auto max-w-6xl'>
-            <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive('') }} ></Link>
+            <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive(''); window.scrollTo(0,0,); }} ></Link>
         </div>
 
       </nav>
