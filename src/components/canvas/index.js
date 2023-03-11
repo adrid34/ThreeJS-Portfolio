0103ae0a5d075b2b7c195ecae061df6e2f339b04
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-11 18:41:15 +0100

    import export fix

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
+import SoullesFace from "./SoullesFace";
 import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { EarthCanvas, BallCanvas, SoullesFace, StarsCanvas };
