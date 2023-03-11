694552a52c246d7fcc413c8a81e3edc986515c25
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-11 17:29:35 +0100

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,19 +17,33 @@ const Navbar = () => {
 
             <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive(''); window.scrollTo(0,0,); }} >
               <img src={logo} alt="logo" className='w-24 h-24 object-contain' />
-              <p className='font-bold cursor-pointer text-[20px]'> Kamil <span className='sm:block hidden'>JavaScript Developer</span> </p>
+              <p className='font-bold cursor-pointer text-[20px] flex'> Kamil &nbsp;  <span className='sm:block hidden'> | JavaScript Developer</span> </p>
             </Link>
 
             <ul className='list-none hidden sm:flex flex-row gap-10'>
                 {navLinks.map( (navLink)=> (
-                  <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-800' : 'text-secondary' } hover:text-zinc-900 text-[20px] cursor-pointer` } onClick={ ()=> setActive(navLink.title) } >
+                  <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-200' : 'text-secondary' } hover:text-zinc-900 text-[20px] cursor-pointer` } onClick={ ()=> setActive(navLink.title) } >
                     <a href={` #${navLink.id} `}> {navLink.title} </a>
                   </li>
                 ) )}
             </ul>
 
             <div className='sm:hidden flex flex-1 justify-end items-center'>
-                <img src={menu} alt="menu" className='w-[24px] h-[24px] object-contain cursor-pointer' onClick={ ()=> setToggle(!toggle) } />
+
+                <img src={ toggle ? close : menu } alt="menu" className='w-[24px] h-[24px] object-contain cursor-pointer' onClick={ ()=> setToggle(!toggle) } />
+
+                <div className={ `${!toggle ? 'hidden' : 'flex'} p-7 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-2xl` }>
+
+                  <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                    {navLinks.map( (navLink)=> (
+                      <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-200' : 'text-secondary' } text-[16px] cursor-pointer` } onClick={ ()=> { setActive(navLink.title); setToggle(!toggle) } } >
+                        <a href={` #${navLink.id} `}> {navLink.title} </a>
+                      </li>
+                    ) )}
+                  </ul>
+
+                </div>
+
             </div>
 
         </div>
