d8035c9262af24321f603bfc061add68b6f5a0ff
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-11 17:29:21 +0100

    background image deleted

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
         <div className='relative z-0 bg-primary text-gray-900'>
 
-          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <div className=' bg-cover bg-no-repeat bg-center'>
               <Navbar />
               <Hero />
           </div>
