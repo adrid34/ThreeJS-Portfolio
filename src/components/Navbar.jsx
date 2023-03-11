70ee438a253954e2b4123e972478e6c128758180
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-11 16:57:07 +0100

    links for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,23 @@ const Navbar = () => {
 
   return (
 
-      <nav className={`${styles.paddingX} fixed top-0 w-full items-center py-5 z-20`}>
+      <nav className={`${styles.paddingX} fixed top-0 w-full flex items-center py-4 z-30 bg-primary`}>
         
         <div className='w-full flex justify-between items-center mx-auto max-w-6xl'>
-            <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive(''); window.scrollTo(0,0,); }} ></Link>
+
+            <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive(''); window.scrollTo(0,0,); }} >
+              <img src={logo} alt="logo" className='w-24 h-24 object-contain' />
+              <p className='font-bold cursor-pointer text-[20px]'> Kamil <span className='sm:block hidden'>JavaScript Developer</span> </p>
+            </Link>
+
+            <ul className='list-none hidden sm:flex flex-row gap-10'>
+                {navLinks.map( (navLink)=> (
+                  <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-800' : 'text-secondary' } hover:text-zinc-900 text-[20px] cursor-pointer` } onClick={ ()=> setActive(navLink.title) } >
+                    <a href={` #${navLink.id} `}> {navLink.title} </a>
+                  </li>
+                ) )}
+            </ul>
+
         </div>
 
       </nav>
