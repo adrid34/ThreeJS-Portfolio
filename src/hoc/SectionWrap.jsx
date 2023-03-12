cb8fe472b03f28df01f67e408f8b355b2cc3178b
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-12 23:00:49 +0100

    Wrapper elements

--- src/hoc/SectionWrap.jsx
+++ src/hoc/SectionWrap.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { styles } from '../styles'
+import { staggerContainer } from '../utilities/motion'
+import { motion } from 'framer-motion'
 
-const SectionWrap = () => {
+const SectionWrap = (Component, idName) => function HOC() {
 
-  return (
+    return (
 
-    <div>SectionWrap</div>
-    
-  )
+        <motion.section variants={ staggerContainer() } initial='hidden' whileInView='show' viewport={{ once: true, amount: 0.25, }} className={` ${styles.padding} max-w-7xl mx-auto relative z-0 `}>
+            <Component />
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
+        </motion.section>
+
+    )
+
 }
+
+
+
 
 export default SectionWrap
