a91724003ce73a9dbd5c97b060e9e27fdd4920f8
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-12 14:16:56 +0100

    responsive design

--- src/components/canvas/SoullesFace.jsx
+++ src/components/canvas/SoullesFace.jsx
@@ -4,7 +4,7 @@ import React, { Suspense, useEffect, useState } from 'react'
 import CanvasLoader from '../Loader'
 
 
-const SoullesFace = () => {
+const SoullesFace = ({ isMobile }) => {
 
   const soul = useGLTF('./bm86_portable_pc/scene.gltf');
 
@@ -14,7 +14,7 @@ const SoullesFace = () => {
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
       <spotLight position={[ -20, 50, 10 ]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-      <primitive object={soul.scene} scale={10} position={[ 0, -3.25, -1.5 ]} rotation={[ -0.01, -0.2, -0.1 ]} />
+      <primitive object={soul.scene} scale={ isMobile ? 8 : 12} position={ isMobile ? [0, -3, -2.2] : [ 0, -3.25, -1.5 ]} rotation={[ -0.01, -0.2, -0.1 ]} />
     </mesh>
 
   )
@@ -25,13 +25,33 @@ const SoulCanvas = () => {
 
   const [isMobile, setIsMobile] = useState(false);
 
+  useEffect( ()=> {
+
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = function(e) {
+
+      setIsMobile(e.matches);
+
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    
+    return () => { mediaQuery.removeEventListener('change', handleMediaQueryChange); }
+
+  }, [])
+
+
+
   return (
 
     <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       
       <Suspense fallback={ <CanvasLoader /> }>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <SoullesFace />
+        <SoullesFace isMobile = {isMobile} />
       </Suspense>
 
       <Preload all />
