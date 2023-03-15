c586f6b06bd23754923f29dd3be0b42ddd6a7329
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-15 17:43:37 +0100

    Contact section initial components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,14 +26,46 @@ const Contact = () => {
   }
 
   const handleSubmit = function(e) {
-    
+
   }
 
   return (
 
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+
+      <motion.div variants={ slideIn('left', 'tween', 0.2, 1) } className='flex-[0.75] p-8 rounded-2xl bg-[#ECF0F1] shadow-card' >
+        
+        <p className={styles.sectionSubText} >Get in touch</p>
+        <h3 className={styles.sectionHeadText} >Contact.</h3>
+
+        <form ref={formRef} onSubmit={ handleSubmit } className='mt-12 flex flex-col gap-8' >
+
+          <label className='flex flex-col'>
+            <span className='mb-4 font-medium'>Your name</span>
+            <input type="text" name='name' value={form.name} placeholder="What's your name?" onChange={ handleChange } className='py-4 px-6 bg-transparent rounded-lg outline-none border-none shadow-input' />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='mb-4 font-medium'>Your email</span>
+            <input type="email" name='email' value={form.email} placeholder="What's your email?" onChange={ handleChange } className='py-4 px-6 bg-transparent rounded-lg outline-none border-none shadow-input' />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='mb-4 font-medium'>Your message</span>
+            <textarea rows={6} type="text" name='message' value={form.message} placeholder="What's your message to me?" onChange={ handleChange } className='py-4 px-6 bg-transparent rounded-lg outline-none border-none shadow-input' />
+          </label>
+
+          <button type='submit' className='py-3 px-10 outline-none w-fit font-medium shadow-input rounded-xl'>
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+
+        </form>
+
+      </motion.div>
+
+    </div>
 
   )
 }
 
-export default Contact
+export default SectionWrap(Contact, 'contact') 
