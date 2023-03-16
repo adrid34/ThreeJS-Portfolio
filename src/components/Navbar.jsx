3e7974cf3b4232452188e83da0d7c2d537dee105
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-16 19:29:56 +0100

    Navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,12 +17,12 @@ const Navbar = () => {
 
             <Link to='/' className='flex items-center gap-2' onClick={ ()=> { setActive(''); window.scrollTo(0,0,); }} >
               <img src={logo} alt="logo" className='w-24 h-24 object-contain' />
-              <p className='font-bold cursor-pointer text-[20px] flex'> Kamil &nbsp;  <span className='sm:block hidden'> | FrontEnd Developer</span> </p>
+              <p className='font-bold cursor-pointer text-[20px] flex'> Kamil &nbsp;<span className='sm:block hidden'>| FrontEnd Developer</span> </p>
             </Link>
 
             <ul className='list-none hidden sm:flex flex-row gap-10'>
                 {navLinks.map( (navLink)=> (
-                  <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-700' : 'text-secondary' } hover:text-zinc-900 text-[20px] cursor-pointer` } onClick={ ()=> setActive(navLink.title) } >
+                  <li key={navLink.id} className={ `${ active === navLink.title ? 'font-semibold' : 'text-slate-900' } hover:scale-105 text-[20px] cursor-pointer` } onClick={ ()=> setActive(navLink.title) } >
                     <a href={` #${navLink.id} `}> {navLink.title} </a>
                   </li>
                 ) )}
@@ -32,11 +32,11 @@ const Navbar = () => {
 
                 <img src={ toggle ? close : menu } alt="menu" className='w-[24px] h-[24px] object-contain cursor-pointer' onClick={ ()=> setToggle(!toggle) } />
 
-                <div className={ `${!toggle ? 'hidden' : 'flex'} p-7 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-2xl` }>
+                <div className={ `${!toggle ? 'hidden' : 'flex'} p-7 bg-[#ECF0F1] shadow-card absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-2xl` }>
 
                   <ul className='list-none flex justify-end items-start flex-col gap-4'>
                     {navLinks.map( (navLink)=> (
-                      <li key={navLink.id} className={ `${ active === navLink.title ? 'text-slate-200' : 'text-secondary' } text-[16px] cursor-pointer` } onClick={ ()=> { setActive(navLink.title); setToggle(!toggle) } } >
+                      <li key={navLink.id} className={ `${ active === navLink.title ? 'font-semibold' : 'text-slate-800' } text-[16px] cursor-pointer` } onClick={ ()=> { setActive(navLink.title); setToggle(!toggle) } } >
                         <a href={` #${navLink.id} `}> {navLink.title} </a>
                       </li>
                     ) )}
