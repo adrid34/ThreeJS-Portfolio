3fb50d3faf1370ca985059ff2dd5807ae728d415
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-16 19:30:11 +0100

    Hero section fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,11 @@ const Hero = () => {
 
         <div className='flex flex-col justify-center items-center mt-8'>
           <div className='w-5 h-5 rounded-full bg-[#283747]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-1 sm:h-80 h-40 violet-dark-gradient' />
         </div>
 
         <div>
-          <h1 className={` ${styles.heroHeadText} `}>Hi, I'm <span className='text-[#4A235A]'>Kamil</span> </h1>
+          <h1 className={` ${styles.heroHeadText} `}>Hi, I'm <span className='text-[85px] '>Kamil</span> </h1>
           <p className={ `${styles.heroSubText} mt-4 text-slate-800` }>I am a student, software developer <br className='sm:block hidden' /> and a motivated learner!</p>
         </div>
 
