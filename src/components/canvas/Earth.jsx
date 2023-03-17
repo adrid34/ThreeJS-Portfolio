1a07f90faab16df66c93c71f2bd764cd0fe79773
Adrian Madrid <adrianmadrid5245@gmail.com>
2023-03-17 16:11:34 +0100

    Earth 3d object rendering

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,11 +5,17 @@ import CanvasLoader from '../Loader'
 
 const Earth = () => {
 
-  const earth = useGLTF('./stylized_planet/scene.gltf');
+  const earth = useGLTF('./world_earth_planet/scene.gltf');
 
   return (
 
-    <primitive object={ earth.scene } scale={2} position-y={0} rotation-y={0} />
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <spotLight position={[ -20, 50, 10 ]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+    <primitive object={ earth.scene } scale={0.07} position-y={0} rotation-y={0} />
+
+    </mesh>
 
   )
 }
@@ -19,7 +25,7 @@ const EarthCanvas = () => {
 
   return (
 
-    <Canvas shadows frameloop='demand'  gl={{ preserveDrawingBuffer: true, }} camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }} >
+    <Canvas shadows frameloop='demand' dpr={[1, 2]} gl={{ preserveDrawingBuffer: true, }} camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }} >
       
       <Suspense fallback={ <CanvasLoader /> } >
         <OrbitControls autoRotate enableZoom={false} maxPolarAngle={ Math.PI / 2 } minPolarAngle={ Math.PI / 2 } />
